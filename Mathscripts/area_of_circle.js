00f36b6edf8a84d64c62fee09af6042a7d7b6afe
Deavion Danielle <d.danielle@Deavions-MBP.home>
2022-10-18 10:36:21 -0400

    Added area of a circle file

---
+++ Mathscripts/area_of_circle.js
@@ -0,0 +1,25 @@
+// sample input 
+var radius = 4; 
+var pie = 3.14;
+
+// calculation 
+var area = pie * radius**2;
+
+//output 
+console.log ("The area of a circle with radius 4 is " + area) ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

d2be64e574e81c6c952af997533871efaf10c64b
Deavion Danielle <d.danielle@Deavions-MBP.home>
2022-10-18 14:15:24 -0400

    Made another correction for file

--- Mathscripts/tiles.js
+++ Mathscripts/tiles.js
@@ -1,16 +1,12 @@
-var a = 3.2808;
-//Actual dimensions of the room, using meters multiplied by foot
-var length = 12 * a; // length = 39
-var width = 6 * a;   // width = 19
-var lengthFeet = Math.trunc(length);
-var widthFeet = Math.trunc(width);
+// sample input 
+var length = 39 
+var width = 19  
+
 
 //calculations
 var roomDimension = Math.trunc(length * width)
 var boxes = Math.trunc(length * width);
 
 //output
-console.log(lengthFeet);
-console.log(widthFeet);
-console.log("The room dimensions " + room)
+console.log("The room dimensions " + roomDimension + " square foot ");
 console.log("The boxes needed to tile the room " + boxes);

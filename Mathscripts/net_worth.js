b7aaa13c2ce8700f0b43ce81d4c45a89ca009ce7
Deavion Danielle <d.danielle@Deavions-MBP.home>
2022-10-18 09:50:29 -0400

    Added new folder and networth file

---
+++ Mathscripts/net_worth.js
@@ -0,0 +1,14 @@
+// sample input 
+var assets = 350000;
+var liabilities = 100000;
+
+// calculation
+var networth = assets - liabilities;
+
+//output
+console.log("Your networth is " + networth) ;
+
+
+
+
+

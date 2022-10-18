17aa2194b787964afa99935e3e07ae1af08e5da2
Deavion Danielle <d.danielle@Deavions-MBP.home>
2022-10-18 10:27:44 -0400

    Added tip amount file

---
+++ Mathscripts/tip_amount.js
@@ -0,0 +1,9 @@
+// sample input 
+var foodCost = 100;
+var tip = 0.12;
+
+// calculation 
+var totalDue = foodCost * tip; 
+
+// output 
+console.log("The tip on a $100 food bill is $ " + totalDue) ;

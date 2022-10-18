852437e07c4f5033d5ed2c5143e83774b3bbbffd
Deavion Danielle <d.danielle@Deavions-MBP.home>
2022-10-18 13:56:10 -0400

    All 6 new exercises w/correction

--- Mathscripts/tiles.js
+++ Mathscripts/tiles.js
@@ -1,14 +1,16 @@
 var a = 3.2808;
 //Actual dimensions of the room, using meters multiplied by foot
-var length = 12 * a;
-var width = 6 * a;
+var length = 12 * a; // length = 39
+var width = 6 * a;   // width = 19
 var lengthFeet = Math.trunc(length);
 var widthFeet = Math.trunc(width);
-//calculations
 
+//calculations
+var roomDimension = Math.trunc(length * width)
 var boxes = Math.trunc(length * width);
 
 //output
 console.log(lengthFeet);
 console.log(widthFeet);
+console.log("The room dimensions " + room)
 console.log("The boxes needed to tile the room " + boxes);

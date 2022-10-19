a29c90399523a9e050f3c22c2c206863436ffc52
Deavion Danielle <d.danielle@Deavions-MBP.home>
2022-10-19 13:30:00 -0400

    Added pay exercise

--- Mathscripts/tiles.js
+++ Mathscripts/tiles.js
@@ -4,9 +4,10 @@ var width = 19
 
 
 //calculations
-var roomDimension = Math.trunc(length * width)
-var boxes = Math.trunc(length * width);
+var roomDimension = Math.trunc(length * width);
+var roomDimensionWithOverage = roomDimension + (roomDimension * 0.1);
+var boxes = Math.trunc(roomDimension / 12);
 
 //output
-console.log("The room dimensions " + roomDimension + " square foot ");
+console.log("The room dimensions " + roomDimensionWithOverage + " square foot ");
 console.log("The boxes needed to tile the room " + boxes);

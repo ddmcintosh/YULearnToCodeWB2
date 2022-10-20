df8283b21c19ed0453ded43847a15f54074fcdb1
Your Name <you@example.com>
2022-10-20 14:32:01 -0400

    greet by name exercise

---
+++ GreetByName/scripts/index.js
@@ -0,0 +1,18 @@
+"use strict";
+window.onload =init;
+function init()
+{
+    const onGreetUserBtnClicked = document.getElementById("greetButton");
+    greetButton.onclick = greetButtonOnClick;
+}
+function OnGreetUserBtnOnClick()
+{
+    alert(" Hello userNameHere ");
+    console.log(" Hello usernameHere an alert was created");
+    let onGreetUserBtnClicked = document.getElementById(" onGreetUserBtnClicked ");
+    messageonGreetUserBtnClicked.innerHTML = "Hello Username here";
+}
+
+
+
+
